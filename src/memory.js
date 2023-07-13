c6242a7af034ada0387dcb619853ae163017c0b8
Agathe Lurquin <agathe.lurquin@essca.eu>
2023-07-13 23:03:51 +0200

    iteration 2 done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,45 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards){
+      return undefined
+    }
+
+    const randomOrders = [];
+    for (const card of this.cards){
+      const pickedCardId = Math.floor(Math.random()*(this.cards.length-1));
+      const pickedCard = this.cards[pickedCardId];
+      randomOrders.push(pickedCard);
+      this.cards.splice(pickedCardId, 1);
+    }
+    return randomOrders
+
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    console.log(card1, card2)
+    this.pairsClicked++;
+    if(card1 === card2){
+      this.pairsGuessed++
+      console.log('yep')
+      return true
+    } else {
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    let pairs = this.cards.length;
+     if(this.pairsGuessed === this.cards.length) {
+      return true
+    } else if (this.pairsClicked === 0 || this.pairsGuessed < this.cards.length ){
+      return false
+    }
   }
 }
